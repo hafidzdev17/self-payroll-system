c78b078a9edfa95b6de38bec3df22db40e9cb642
hafidzdev17 <do.crazy192@gmail.com>
2023-05-15 07:49:33 +0700

    feat: update validation in company repository

--- repository/company.go
+++ repository/company.go
@@ -44,7 +44,13 @@ func (c *companyRepository) CreateOrUpdate(ctx context.Context, company *model.C
 	}
 
 	// TODO: tuliskan baris code untuk update data company
-
+	if err := c.Cfg.Database().WithContext(ctx).Debug().
+		Model(&companyModel).
+		Where("id = ?", company.ID).
+		Updates(company).
+		Find(company).Error; err != nil {
+		return nil, err
+	}
 	return companyModel, nil
 }
 
@@ -55,10 +61,10 @@ func (c *companyRepository) DebitBalance(ctx context.Context, amount int, note s
 	}
 
 	// TODO: tuliskan baris code untuk mengurangi balance
+	company.Balance -= amount
 
 	if err := c.Cfg.Database().WithContext(ctx).Model(company).Updates(company).Find(company).Error; err != nil {
 		return err
-
 	}
 
 	if err := c.Cfg.Database().WithContext(ctx).Create(&model.Transaction{
